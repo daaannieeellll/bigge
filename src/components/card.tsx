fa83ea224ed8ea8d2ac1eca4f6e346e276136743
Daniel <daniel@ospr.nl>
2022-09-12 16:18:52 +0200

    feat: handle two dimensional dragging

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -23,6 +23,18 @@ interface ICardProps {
   text: string;
 }
 
+const normalize = (dx: number, dy: number, dirX: number, dirY: number) => {
+  // avoid zero-division
+  if (dx === 0 && dy === 0) return [dx, dy];
+
+  const x = Math.abs(dx);
+  const y = Math.abs(dy);
+
+  // divide by the biggest factor
+  if (x > y) return [dirX, (y / x) * dirY];
+  return [(x / y) * dirX, dirY];
+};
+
 const Card = ({ type, text, onDiscard: discardHandler }: ICardProps) => {
   // The set flags all the cards that are flicked out
   const [discarded, setDiscarded] = useState(false);
@@ -36,28 +48,28 @@ const Card = ({ type, text, onDiscard: discardHandler }: ICardProps) => {
 
   // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
   const bind = useDrag(
-    ({ down, movement: [mx], direction: [xDir], velocity }) => {
+    ({ down, movement: [mx, my], direction: [dirX, dirY], velocity }) => {
       // If you flick hard enough it should trigger the card to fly out
       const trigger = velocity[0] > 0.2;
-
-      // Direction should either point left or right
-      const dir = xDir < 0 ? -1 : 1;
 
       // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
       if (!down && trigger) _discarded = true;
 
       api.start(() => {
         // When a card is gone it flys out left or right, otherwise goes back to zero
-        const x = _discarded ? (0 + window.innerWidth) * dir : down ? mx : 0;
+        const [dx, dy] = normalize(mx, my, dirX, dirY);
+        const x = _discarded ? window.innerWidth * dx : down ? mx : 0;
+        const y = _discarded ? window.innerHeight * dy : down ? my : 0;
 
         // How much the card tilts, flicking it harder makes it rotate faster
-        const rot = mx / 100 + (_discarded ? dir * 40 * velocity[0] : 0);
+        const rot = mx / 100 + (_discarded ? dirX * 40 * velocity[0] : 0);
 
         // Active cards lift up a bit
         const scale = down ? 1.03 : 1;
 
         return {
           x,
+          y,
           rot,
           scale,
           delay: undefined,
