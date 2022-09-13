618ac2d19c72548d4db583f966c5549b0ff6a6f5
Daniel <daniel@ospr.nl>
2022-09-13 13:51:11 +0200

    style: remove commented code

--- src/components/cardData.tsx
+++ src/components/cardData.tsx
@@ -12,11 +12,9 @@ const CardData = ({ type, text }: ICardDataProps) => {
     >
       <div className='w-3/4 select-none'>
         <img src={`/images/types/${type}.svg`} alt={type} />
-        {/* <img src={`/images/types/De Big.svg`} alt='De Big' /> */}
       </div>
       <div className='text-[2vh] text-center w-4/5 h-1/2 select-none'>
         <p>{text}</p>
-        {/* <p>text</p> */}
       </div>
     </div>
   );
