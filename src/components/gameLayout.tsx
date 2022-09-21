95c75852c597d6c19850a0775fda298dab5ee27c
Daniel <daniel@ospr.nl>
2022-09-21 20:57:30 +0200

    style: use parentheses instead of block braces

--- src/components/gameLayout.tsx
+++ src/components/gameLayout.tsx
@@ -2,12 +2,10 @@ import GameContextProvider from "@/context/gameContext";
 import GameBackground from "@/components/gameBackground";
 import type { ReactNode } from "react";
 
-const GameLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <GameContextProvider>
-      <GameBackground>{children}</GameBackground>
-    </GameContextProvider>
-  );
-};
+const GameLayout = ({ children }: { children: ReactNode }) => (
+  <GameContextProvider>
+    <GameBackground>{children}</GameBackground>
+  </GameContextProvider>
+);
 
 export default GameLayout;
