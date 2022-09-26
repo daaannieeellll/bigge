f584f13f1fc62c24d6cad0335f99525addf7d10c
Daniel <daniel@ospr.nl>
2022-09-26 14:29:07 +0200

    feat: use custom worker

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 
 const withPWA = require("next-pwa")({
   dest: "public",
+  swSrc: "src/worker/sw.js",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({
