f97b9fda2184bb94cc47ea7a8f09da33399e0cfc
Daniel <daniel@ospr.nl>
2022-09-27 21:31:44 +0200

    refactor: replace mapper

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,56 +10,105 @@ export default function Document() {
         />
 
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/images/icons/72x72.png' />
         <meta name='theme-color' content='#F98F8F' />
+        <link rel='icon' href='/images/icons/72x72.png' />
 
         {/* Apple specific tags */}
+        <meta name='apple-mobile-web-app-capable' content='yes' />
         <link
           rel='apple-touch-icon'
           href='/images/icons/1024x1024-maskable.png'
         />
-
-        <meta name='apple-mobile-web-app-capable' content='yes' />
-        {/* map supported splash screens to link tag */}
-        {[
-          /* 12.9" iPad Pro */
-          { width: 1024, height: 1366, ratio: 2 },
-          /* 10.5" iPad Pro, 11" iPad Pro */
-          { width: 834, height: 1194, ratio: 2 },
-          /* 10.5" iPad Air */
-          { width: 834, height: 1112, ratio: 2 },
-          /* 10.2" iPad */
-          { width: 810, height: 1080, ratio: 2 },
-          /* 9.7" iPad, 9.7" iPad Air */
-          { width: 768, height: 1024, ratio: 2 },
-          /* iPhone 14 Pro Max */
-          { width: 430, height: 932, ratio: 3 },
-          /* iPhone 12 Pro Max, iPhone 13 Pro Max, iPhone 14 Plus */
-          { width: 428, height: 926, ratio: 3 },
-          /* iPhone Xs Max, iPhone 11 Pro Max */
-          { width: 414, height: 896, ratio: 3 },
-          /* iPhone XR, iPhone 11 */
-          { width: 414, height: 896, ratio: 2 },
-          /* iPhone 6s Plus, iPhone 7 Plus, iPhone 8 Plus */
-          { width: 414, height: 736, ratio: 3 },
-          /* iPhone 14 Pro */
-          { width: 393, height: 852, ratio: 3 },
-          /* iPhone 12, iPhone 12 Pro, iPhone 13, iPhone 13 Pro, iPhone 14 */
-          { width: 390, height: 844, ratio: 3 },
-          /* iPhone X, iPhone Xs, iPhone 11 Pro, iPhone 12 mini */
-          { width: 375, height: 812, ratio: 3 },
-          /* iPhone 6, iPhone 6s, iPhone 7, iPhone 8, 4.7" iPhone SE */
-          { width: 375, height: 667, ratio: 2 },
-          /* iPod touch 5th gen, 4" iPhone SE  */
-          { width: 320, height: 568, ratio: 2 },
-        ].map(({ width, height, ratio }, idx) => (
-          <link
-            key={idx}
-            rel='apple-mobile-startup-image'
-            href={`/images/icons/${width}x${height}@${ratio}x.png`}
-            media={`(device-width: ${width}px) and (device-height: ${height}px) and (-webkit-device-pixel-ratio: ${ratio})`}
-          />
-        ))}
+        {/* 12.9" iPad Pro */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/1024x1366@2x.png'
+          media='(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        {/* 10.5" iPad Pro, 11" iPad Pro */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/834x1194@2x.png'
+          media='(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        {/* 10.5" iPad Air */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/834x1112@2x.png'
+          media='(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        {/* 10.2" iPad */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/810x1080@2x.png'
+          media='(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        {/* 9.7" iPad, 9.7" iPad Air */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/768x1024@2x.png'
+          media='(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        {/* iPhone 14 Pro Max */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/430x932@3x.png'
+          media='(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+        />
+        {/* iPhone 12 Pro Max, iPhone 13 Pro Max, iPhone 14 Plus */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/428x926@3x.png'
+          media='(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+        />
+        {/* iPhone Xs Max, iPhone 11 Pro Max */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/414x896@3x.png'
+          media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+        />
+        {/* iPhone XR, iPhone 11 */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/414x896@2x.png'
+          media='(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        {/* iPhone 6s Plus, iPhone 7 Plus, iPhone 8 Plus */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/414x736@3x.png'
+          media='(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+        />
+        {/* iPhone 14 Pro */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/393x852@3x.png'
+          media='(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+        />
+        {/* iPhone 12, iPhone 12 Pro, iPhone 13, iPhone 13 Pro, iPhone 14 */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/390x884@3x.png'
+          media='(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+        />
+        {/* iPhone X, iPhone Xs, iPhone 11 Pro, iPhone 12 mini */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/375x812@3x.png'
+          media='(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+        />
+        {/* iPhone 6, iPhone 6s, iPhone 7, iPhone 8, 4.7" iPhone SE */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/375x667@2x.png'
+          media='(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
+        {/* iPod touch 5th gen, 4" iPhone SE  */}
+        <link
+          rel='apple-touch-startup-image'
+          href='/images/splash/320x568@2x.png'
+          media='(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)'
+        />
       </Head>
       <body>
         <Main />
