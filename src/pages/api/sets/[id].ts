d414978dbf11414053472c39ff2697dd36009065
Daniel <daniel@ospr.nl>
2022-10-01 00:18:26 +0200

    fix: set correct status code (201) for POST method

--- src/pages/api/sets/[id].ts
+++ src/pages/api/sets/[id].ts
@@ -8,19 +8,27 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   let statusCode = 200;
   let responseData;
+
   try {
     const { id } = req.query;
-    if (typeof id !== "string") statusCode = 400;
-    else if (req.method === "GET") responseData = await getSets(id);
-    else if (req.method === "POST") await postSets(req.body, id);
-    else if (req.method === "PUT") await putSets(req.body, id);
-    else if (req.method === "DELETE") await deleteSets(id);
-    else statusCode = 405;
+    if (typeof id !== "string") {
+      statusCode = 400;
+    } else if (req.method === "GET") {
+      responseData = await getSets(id);
+    } else if (req.method === "POST") {
+      await postSets(req.body, id);
+      statusCode = 201;
+    } else if (req.method === "PUT") {
+      await putSets(req.body, id);
+    } else if (req.method === "DELETE") {
+      await deleteSets(id);
+    } else statusCode = 405;
   } catch (e) {
     const error = e as ApiError;
     statusCode = error.statusCode;
     responseData = { error: error.message };
   }
+
   res.statusCode = statusCode;
   res.json(responseData);
 };
