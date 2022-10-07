73a01751c1dfd5bdbee3bbdc0216db9d9fdb5b53
Daniel <daniel@ospr.nl>
2022-10-07 23:17:37 +0200

    style: remove comment

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -65,7 +65,6 @@ export const authOptions: NextAuthOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
   pages: { signIn: routes.signIn, signOut: routes.signOut },
-  // debug: true,
 };
 
 const nextAuthHandler: NextApiHandler = (
