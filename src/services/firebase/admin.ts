4ba5a6119d8b89f47bffd2c0cd6a774b8bd41c18
Daniel <daniel@ospr.nl>
2022-10-07 22:52:59 +0200

    refactor: rename environment variable

--- src/services/firebase/admin.ts
+++ src/services/firebase/admin.ts
@@ -2,9 +2,7 @@ import { initializeApp, getApps } from "firebase-admin/app";
 import { credential } from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
 
-const serviceAccount = JSON.parse(
-  String(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
-);
+const serviceAccount = JSON.parse(String(process.env.FIREBASE_SERVICE_ACCOUNT));
 const firebaseConfig = {
   credential: credential.cert(serviceAccount),
 };
